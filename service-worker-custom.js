e0e49629b6455385e8929a307904d19d5ca03864
Daniele Sforza <danielesforza@gmail.com>
2018-10-15 12:18:10 +0200

    Updates

---
+++ service-worker-custom.js
@@ -0,0 +1,29 @@
+console.log('My custom service worker')
+
+const staticCacheName = 'neighBEERhood-Map-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        './index.html',
+        './index.js',
+        './css/*.css',
+        './media/*.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
