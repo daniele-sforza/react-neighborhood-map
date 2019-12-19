a0d9cde120a43c8ef36a9b1a287ac54de0575818
Daniele Sforza <danielesforza@gmail.com>
2019-12-19 16:03:44 +0100

    Updates

---
+++ precache-manifest.1a9b3e042ca20bd81fe19d912ed7bb90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb9fd80cce229359b05faada791b655",
+    "url": "/react-neighborhood-map/index.html"
+  },
+  {
+    "revision": "e1b4b6cf04613e97d24f",
+    "url": "/react-neighborhood-map/static/css/main.86376d17.chunk.css"
+  },
+  {
+    "revision": "78a1c91c871ad323c0e0",
+    "url": "/react-neighborhood-map/static/js/2.86e63b55.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/react-neighborhood-map/static/js/2.86e63b55.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1b4b6cf04613e97d24f",
+    "url": "/react-neighborhood-map/static/js/main.643a51a1.chunk.js"
+  },
+  {
+    "revision": "21bbfa9bd9ba19600702",
+    "url": "/react-neighborhood-map/static/js/runtime-main.72ec8d86.js"
+  },
+  {
+    "revision": "a266a13f71fb4c9e9a0fbee60d3b2369",
+    "url": "/react-neighborhood-map/static/media/logo.a266a13f.svg"
+  }
+]);
